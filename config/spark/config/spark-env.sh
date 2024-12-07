d7312be95ca2d467cac093dc65ca7b58b7ed5dbd
hoangnm1111 <122582790+hoangnm1111@users.noreply.github.com>
2024-12-07 19:52:11 +0700

    Add files via upload

---
+++ config/spark/config/spark-env.sh
@@ -0,0 +1,6 @@
+export HIVE_SERVER2_THRIFT_BIND_HOST=localhost # IMPORTANT: chu y doi host
+export SPARK_DEFAULT_JARS=/home/hoang/workarea/software/spark-3.3.0-bin-hadoop3/jars/*  # IMPORTANT: chu y doi duong dan
+export SPARK_LOCAL_IP=localhost  # IMPORTANT: chu y doi host
+export SPARK_PUBLIC_DNS=localhost # IMPORTANT: chu y doi host
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop # IMPORTANT: chu y doi duong dan
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop # IMPORTANT: chu y doi duong dan
