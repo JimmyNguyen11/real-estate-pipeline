325bdfc447e1d2a13a20eed2b41ec9fdf2d7beb3
hoangnm1111 <122582790+hoangnm1111@users.noreply.github.com>
2024-12-12 21:29:08 +0700

    Update

---
+++ real_estate_pipeline/data/sql/demo/ext_source_demo1.sql
@@ -0,0 +1,15 @@
+SELECT
+    du_an,
+    price_VND,
+    location,
+    status,
+    area,
+    price_m2,
+    toilet,
+    room,
+    doc,
+    type,
+    So_tang,
+    furnishing,
+    created_date
+FROM demo1
